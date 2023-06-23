4fea0cc23f60d8a8567f514e6b664fae0b9294c7
KasperVanDenEnden <kaspervde1996@hotmail.com>
2023-06-23 15:44:18 +0200

    Configured initial app and identity DB

---
+++ Infrastructure/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Tables from domain Classes Example
+        // public DbSet<Boardgame> Boardgame { get; set; } = null!;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
